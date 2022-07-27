b40f9365cd56620180ef17bbf5753c9f0e61c272
Jee2110 <91604658+Jee2110@users.noreply.github.com>
2022-07-27 22:20:59 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
-*sᴛᴀᴛᴜs:* single 
+*ɴᴀᴍᴇ:* -
+*ᴀɢᴇ:* -
+*sᴛᴀᴛᴜs:* Single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* Private
+*ᴀᴅᴅʀᴇss:* Tangerang
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
